3725f4dca78a31f261c84167bc628619f0660866
Ramandeep Singh <rmongia@instartlogic.com>
2014-07-31 16:57:04 +0530

    gnome-terminal config backup
    - ~/.gconf/apps/gnome-terminal
    
    Reviewed by:
    Review:
    Testing:
    Tickets: PBL-
    Backport:

--- schedule_log_shipper_client_downtime.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -x
-export PYTHONPATH=$PYTHONPATH:/home/rmongia/kido
-
-SERVICE_NAME="Logshipper Client Status"
-if [ $# -eq 1 ]; then
-  SITE=`echo $1 | cut -d'-' -f1`
-  SERVER=$1
-  /$HOME/kido/tools/production/proxy_controller.py schedule_svc_downtime $SERVER "$SERVICE_NAME"
-fi
